3834c5b5526d3d4276bf0727212491e6dc74147d
Hitesh Gupta T R <hitesh.2457@outlook.com>
2017-05-08 01:16:35 +0530

    Revert "Adding login and home components"
    This reverts commit 68a36cec7d009535bf7dcd8b1536d448e24acbb6.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router'; 
 
-import { LoginModule } from './login/login.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
-import { appRoutingModule } from './app.routing';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landingpage/landingpage.component';
+import { LandingPageNavBarComponent } from './landingpageNavbar/landingpagenavbar.component';
 
 @NgModule({
-imports : [ BrowserModule, LoginModule , HomeModule , appRoutingModule ],
-declarations: [ AppComponent ],
+imports : [ BrowserModule ],
+declarations: [ AppComponent, LandingPageComponent, LandingPageNavBarComponent ],
 bootstrap : [ AppComponent ],
 
 })
--- app/home/model/answer.model.ts
+++
@@ -1,7 +0,0 @@
-export class Answer {
-    public id : number;
-    public answer : string ;
-    public answeredBy: string;
-    public answeredByEmail : string;
-    public upVotesforAnswer : number;
-}
